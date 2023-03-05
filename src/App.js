e084a6c5a463927a63a493766c28d5d4d57cec48
oscar <oscarokumu500@gmail.com>
2023-03-05 18:09:41 +0300

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import FullTaskInfo from "./FormInput/FullTaskInfo";
+import FullInputBox from "./FormInput/FormInputBox";
+import TaskList from "./TaskList/TaskList";
 
-function App() {
+let dummyTask = [
+  {
+    id: "t1",
+    task: "Write to-do",
+    status: "Completed",
+    date: new Date(2021, 5, 17),
+  },
+  {
+    id: "t2",
+    task: "Do it now",
+    status: "Not Completed",
+    date: new Date(2021, 5, 17),
+  },
+];
+
+const App = () => {
+  let [newTask, setNewTask] = useState(dummyTask);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://my-bucket-f9503-default-rtdb.firebaseio.com/task"
+      );
+
+      const data = response.json();
+      setNewTask(data);
+    };
+
+    fetchData();
+  }, []);
+
+  const endTaskInfoHandler = (endTask) => {
+    const sendReq = async () => {
+      const response = await fetch(
+        "https://my-bucket-f9503-default-rtdb.firebaseio.com/task",
+        {
+          method: "POST",
+          body: JSON.Stringify(),
+          headers: { "Content-type " : "application/json" },
+        }
+      );
+    };
+
+    sendReq();
+
+    setNewTask((prevTask) => {
+      return [endTask, ...prevTask];
+    });
+  };
+
+  const onDeleteItemHandler = (todoId) => {
+    setNewTask((prevTodos) => {
+      return prevTodos.filter((todo) => todo.id !== todoId);
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <FullInputBox fullTask={endTaskInfoHandler} />
+      <TaskList items={newTask} onDeleteItem={onDeleteItemHandler} />
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/TaskList/TaskList.js
@@ -0,0 +1,22 @@
+import React from "react";
+import style from "./TaskList.module.css";
+import TaskItem from "./TaskItem";
+
+const TaskList = (props) => {
+
+  return (
+    <div>
+      <ul className={style.tasklist__group}>
+        {props.items.map((newTask) => (<TaskItem 
+        key={newTask.id}
+        task={newTask.task}  
+        date={newTask.date} 
+        status={newTask.status}
+        onClick={props.onDeleteItem.bind(null, newTask.id)}
+        />))}
+        </ul>
+    </div>
+  );
+};
+
+export default TaskList;
